04319d525ef11564dd850070e1de49ac933b19d6
Ruslan Hadyniak <rushad@auditdata.com>
2021-11-15 17:09:52 +0200

    show property name

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace FodyPropertyChangedCSharp
         public event PropertyChangingEventHandler PropertyChanging;
 
         public int Value { get; set; }
+        public string Value2 { get; set; }
     }
 
     class Program
@@ -16,22 +17,23 @@ namespace FodyPropertyChangedCSharp
         static void Main(string[] args)
         {
             var vm = new VM();
-            (vm as INotifyPropertyChanging).PropertyChanging += Program_PropertyChanging;
-            (vm as INotifyPropertyChanged).PropertyChanged += Program_PropertyChanged;
+            vm.PropertyChanging += Program_PropertyChanging;
+            vm.PropertyChanged += Program_PropertyChanged;
             vm.Value = 1;
             vm.Value = 100;
+            vm.Value2 = "new";
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
 
         private static void Program_PropertyChanging(object sender, PropertyChangingEventArgs e)
         {
-            Console.WriteLine("Value changing!");
+            Console.WriteLine($"{e.PropertyName} changing!");
         }
 
         private static void Program_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            Console.WriteLine("Value changed!");
+            Console.WriteLine($"{e.PropertyName} changed!");
         }
     }
 }
